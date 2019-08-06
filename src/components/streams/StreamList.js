5e817626a2d405678ee3bd99636595582ccfc538
gTomasik <grzesiek315@gmail.com>
2019-08-06 16:54:45 +0200

    need improvement

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchStreams } from '../../actions'
 
-const StreamList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+class StreamList extends React.Component{
+  componentDidMount(){
+    this.props.fetchStreams()
+  }
+  
+  render(){ 
+    console.log(this.props.streams)
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
 
-export default StreamList
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) }
+}
+
+export default connect(
+  mapStateToProps, 
+  { fetchStreams }
+)(StreamList)
