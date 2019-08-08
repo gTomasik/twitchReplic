afe16817ee2b347fca2a30b6703f13e5030084e9
gTomasik <grzesiek315@gmail.com>
2019-08-08 15:08:10 +0200

    import error

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ const App = () =>{
         <Header />
         <Route path="/" exact component={StreamList} />
         <Route path="/streams/new" exact component={StreamCreate} />
-        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/edit/:id" exact component={StreamEdit} />
         <Route path="/streams/delete" exact component={StreamDelete} />
         <Route path="/streams/show" exact component={StreamShow} />
       </div>
@@ -25,5 +25,3 @@ const App = () =>{
   )
 } 
 export default App
-//
-//5OSeqavh5swalWHVbPefcy4V
